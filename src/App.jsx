8e4d973a6321b72614ac673b1e573c7266b74bb2
Ishup Bhujel <ishupbhujel168@gmail.com>
2026-01-14 15:19:36 +0545

    fix app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Home from './components/home'
+
 import Hero from './components/Hero'
 import ScrollBoxes from './components/ScrollBoxes'
 import Test from './components/Test'
